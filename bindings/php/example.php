3e5b3f9562fd8cbea3e4fa06e71b15133d9d8cd7
Alan Knowles <alan@roojs.com>
2016-05-26 18:38:13 +0800

    bindings/php/example.php

--- bindings/php/example.php
+++ bindings/php/example.php
@@ -1,8 +1,11 @@
 <?php 
 
 dl('roojspacker.so');
-$p = new roojspacker();
 
+
+$p = new roojspacker();
+// add files to pack
 $p->loadFile("/home/alan/gitlive/roojs1/Function.js");
+// arguments are output filename/debug version filename (empty strings = return output)
 echo $p->pack("","");
 
