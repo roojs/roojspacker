dbd8f45197598fe64d3591744c9f00ed39b0e2c0
Alan Knowles <alan@roojs.com>
2016-05-26 18:38:33 +0800

    bindings/php/example.php

--- bindings/php/example.php
+++ bindings/php/example.php
@@ -7,5 +7,6 @@ $p = new roojspacker();
 // add files to pack
 $p->loadFile("/home/alan/gitlive/roojs1/Function.js");
 // arguments are output filename/debug version filename (empty strings = return output)
+// should throw an exectpionif there is a syntax error.
 echo $p->pack("","");
 
