1e4d285cf965e7636abd836d715291eee25af8eb
Alan Knowles <alan@roojs.com>
2019-02-15 14:55:24 +0800

    sync

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -40,6 +40,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/alan/gitlive/roojspacker/roojspacker/cmake_install.cmake")
+  include("/home/alan/gitlive/roojspacker/bin/cmake_install.cmake")
 
 endif()
 
